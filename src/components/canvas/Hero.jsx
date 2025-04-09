44df7377d78b72f59057d5b9fd1a6cf71270eea9
S <2122989@cognizant.com>
2025-04-09 14:31:41 +0530

    updated portfolio

--- src/components/canvas/Hero.jsx
+++ src/components/canvas/Hero.jsx
@@ -5,10 +5,12 @@ import ComputersCanvas from './Computers'
 
 
 const Hero = () => {
-  return (
+  
+  
+return (
     <section className='relative w-full h-screen mx-auto'>
-        <div className={`${styles.paddingX} absolute z-10 top-[120px] left-0 right-0 max-w-7xl mx-auto flex flex-row items-start gap-5`}
-        style={{ transform: 'translateY(0)' }}>
+        <div className={`${styles.paddingX} absolute z-20 left-0 right-0 max-w-7xl mx-auto flex flex-row items-start gap-5 mb-20`}
+         style={{ top: '70px' }}>
           <div className="flex flex-col justify-center items-center mt-5"> 
             <div className="w-5 h-5 rounded-full bg-[#915eff]" />
             <div className="w-1 sm:h-80 h-40 violet-gradient" />
@@ -21,7 +23,7 @@ const Hero = () => {
             </div>
         </div>
  
-
+      
         <ComputersCanvas />
         
         <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center pt-20'>
