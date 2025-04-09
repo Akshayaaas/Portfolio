49751a66d7fdd3eb5c4ce69dcabd22ca09c4b76d
S <2122989@cognizant.com>
2025-04-09 14:35:12 +0530

    changed size

--- src/components/canvas/Hero.jsx
+++ src/components/canvas/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
 return (
     <section className='relative w-full h-screen mx-auto'>
         <div className={`${styles.paddingX} absolute z-20 left-0 right-0 max-w-7xl mx-auto flex flex-row items-start gap-5 mb-20`}
-         style={{ top: '70px' }}>
+         style={{ top: '35px' }}>
           <div className="flex flex-col justify-center items-center mt-5"> 
             <div className="w-5 h-5 rounded-full bg-[#915eff]" />
             <div className="w-1 sm:h-80 h-40 violet-gradient" />
